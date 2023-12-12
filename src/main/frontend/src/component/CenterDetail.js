62ef0a21ca8a9c62429066ebe14348481af4ca4d
YubinShin <syb0317timo12@gmail.com>
2023-12-12 18:03:38 +0900

    로그인 및 회원가입창 수정
    간격 수정

--- src/main/frontend/src/component/CenterDetail.js
+++ src/main/frontend/src/component/CenterDetail.js
@@ -28,7 +28,7 @@ function CenterDetail() {
       left: '50%',
       transform: 'translate(-50%, -50%)',
       width: 600,
-      bgcolor: 'background.paper',
+      bgcolor: 'white',
       boxShadow: 24,
       p: 4,
       borderRadius: '8px',
--- src/main/frontend/src/login/Login.js
+++ src/main/frontend/src/login/Login.js
@@ -43,13 +43,8 @@ const Login = () => {
   };
 
   return (
-    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 ">
-          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-            <img
-              className="mx-auto h-20 w-auto"
-              src="img/trashbox.png"
-              alt="Logo"
-            />
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8 mt-20">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm mt-20">
             <h2 className="mt-5 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
               구해줘! 지구🌏
             </h2>
